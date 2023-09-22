7d89300789f3595df6dbdc2a0e376bc5c31a0e2e
tracer-ics2023 <tracer@njuics.org>
2023-09-22 12:28:11 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dreamtouch-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:28:11 up 35 min,  1 user,  load average: 0.83, 0.73, 0.73

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -47,9 +47,9 @@ static struct rule {
     {"\\+", '+'},      // plus
     {"==", TK_EQ},     // equal
     {"[1-9][0-9]*u?", TK_NUM},
-    {"0u?", TK_NUM},
     {"0x[1-9A-F][0-9A-F]*u?", TK_HEX_NUM},
     {"0x0u?", TK_HEX_NUM},
+    {"0u?", TK_NUM},
     {"\\$[\\$a-z0-9]*", TK_REG},
     {"\\-", '-'},
     {"\\(", '('},
