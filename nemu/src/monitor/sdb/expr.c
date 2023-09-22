6c0d2977f2713addc1de4376af7cdb01efe1ddab
tracer-ics2023 <tracer@njuics.org>
2023-09-22 12:32:26 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dreamtouch-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:32:26 up 39 min,  1 user,  load average: 0.61, 0.59, 0.66

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -166,7 +166,8 @@ word_t expr(char *e, bool *success) {
   for (int i = 0; i < nr_token; i++) {
     if (tokens[i].type == '*' &&
         (i == 0 || tokens[i - 1].type == '*' || tokens[i - 1].type == '-' ||
-         tokens[i - 1].type == '+' || tokens[i - 1].type == '/')) {
+         tokens[i - 1].type == '+' || tokens[i - 1].type == '/' ||
+         tokens[i - 1].type == '(')) {
       tokens[i].type = TK_DEREF;
     }
   }
