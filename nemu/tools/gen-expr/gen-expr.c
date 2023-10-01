13b3d44980c7eae5817b19b1ee79312a1e8e1040
tracer-ics2023 <tracer@njuics.org>
2023-10-01 17:55:04 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dreamtouch-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:55:04 up 34 min,  1 user,  load average: 0.41, 0.30, 0.51

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -1,84 +1,103 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan
+ *PSL v2. You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ *KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ *NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
+#include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <assert.h>
 #include <string.h>
+#include <time.h>
 
 // this should be enough
 static char buf[65536] = {};
 static unsigned int buf_cnt = 0;
 static char code_buf[65536 + 128] = {}; // a little larger than `buf`
-static char *code_format =
-"#include <stdio.h>\n"
-"unsigned int main() { "
-"  unsigned result = %s; "
-"  printf(\"%%u\", result); "
-"  return 0; "
-"}";
-static unsigned int choose(unsigned int n){
-  return rand()%n;
+static char *code_format = "#include <stdio.h>\n"
+                           "unsigned int main() { "
+                           "  unsigned result = %s; "
+                           "  printf(\"%%u\", result); "
+                           "  return 0; "
+                           "}";
+static unsigned int choose(unsigned int n) { return rand() % n; }
+static void gen(char c) {
+  buf[buf_cnt] = c;
+  buf_cnt++;
 }
-static void gen(char c){
- 	buf[buf_cnt]=c;
-	buf_cnt++;
-}
-static void gen_num(){
-	unsigned int add_space = choose(3);
-  for(unsigned int i=0;i<add_space;i++)gen(' ');
-  unsigned int num = rand();	
-	if(num==0){
-		gen('0');
-		gen('u');	
-		add_space = choose(3);
-		for(unsigned int i=0;i<add_space;i++)gen(' ');
-		return;
-	}
-	unsigned int exp = 1;
-  while(num/exp>=10)exp*=10;
-  while(exp>0){
-    gen((num/exp) + '0');
-    num -= (num/exp)*exp;
-    exp/=10; 
+static void gen_num() {
+  unsigned int add_space = choose(3);
+  for (unsigned int i = 0; i < add_space; i++)
+    gen(' ');
+  unsigned int num = rand();
+  if (num == 0) {
+    gen('0');
+    gen('u');
+    add_space = choose(3);
+    for (unsigned int i = 0; i < add_space; i++)
+      gen(' ');
+    return;
   }
-	gen('u');
-	add_space = choose(3);
-  for(unsigned int i=0;i<add_space;i++)gen(' ');
+  unsigned int exp = 1;
+  while (num / exp >= 10)
+    exp *= 10;
+  while (exp > 0) {
+    gen((num / exp) + '0');
+    num -= (num / exp) * exp;
+    exp /= 10;
+  }
+  gen('u');
+  add_space = choose(3);
+  for (unsigned int i = 0; i < add_space; i++)
+    gen(' ');
 }
 
-static void gen_rand_op(){
-  switch(choose(4)){
-    case 0: gen('+');break;
-		case 1: gen('-');break;
-		case 2: gen('*');break;
-		default: gen('/');break;
+static void gen_rand_op() {
+  switch (choose(4)) {
+  case 0:
+    gen('+');
+    break;
+  case 1:
+    gen('-');
+    break;
+  case 2:
+    gen('*');
+    break;
+  default:
+    gen('/');
+    break;
   }
 }
 static void gen_rand_expr(int leng) {
-	if(leng == 0){
-		gen_num();
-		return;
-	}
-	else switch (choose(3)) {
-    case 0: gen_num(); break;
-    case 1: gen('('); gen_rand_expr(leng-1); gen(')'); break;
-    default: gen_rand_expr(leng-1); gen_rand_op(); gen_rand_expr(leng-1); break;
-  }
+  if (leng == 0) {
+    gen_num();
+    return;
+  } else
+    switch (choose(3)) {
+    case 0:
+      gen_num();
+      break;
+    case 1:
+      gen('(');
+      gen_rand_expr(leng - 1);
+      gen(')');
+      break;
+    default:
+      gen_rand_expr(leng - 1);
+      gen_rand_op();
+      gen_rand_expr(leng - 1);
+      break;
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -89,19 +108,20 @@ int main(int argc, char *argv[]) {
     sscanf(argv[1], "%d", &loop);
   }
   unsigned int i;
-  for (i = 0; i < loop; i ++) {
-		memset(buf,0,sizeof(buf));	
-		buf_cnt=0;
-    gen_rand_expr(3);
-		sprintf(code_buf, code_format, buf);
+  for (i = 0; i < loop; i++) {
+    memset(buf, 0, sizeof(buf));
+    buf_cnt = 0;
+    gen_rand_expr(5);
+    sprintf(code_buf, code_format, buf);
 
     FILE *fp = fopen("/tmp/.code.c", "w");
     assert(fp != NULL);
     fputs(code_buf, fp);
     fclose(fp);
 
-		int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
-    if (ret != 0) continue;
+    int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
+    if (ret != 0)
+      continue;
 
     fp = popen("/tmp/.expr", "r");
     assert(fp != NULL);
