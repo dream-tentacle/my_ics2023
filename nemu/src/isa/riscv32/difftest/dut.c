2ec040712c15e9ec712df1e50a59bf6a6622fb74
tracer-ics2023 <tracer@njuics.org>
2023-11-02 21:02:06 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dt-ubuntu 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:02:06 up  1:18,  1 user,  load average: 0.51, 0.47, 0.54

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -20,12 +20,13 @@
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for (int i = 0; i < 32; i++) {
     if (ref_r->gpr[i] != cpu.gpr[i]) {
-      printf("%d is not good\n", i);
+      printf("%d is not good, ref = %x, dut = %x\n", i, ref_r->gpr[i],
+             cpu.gpr[i]);
       return false;
     }
   }
   if (ref_r->pc != cpu.pc) {
-    printf("pc is not good, ref_pc = %x, pc = %x\n", ref_r->pc,
+    printf("pc is not good, ref_pc = %x, dut_pc = %x\n", ref_r->pc,
            cpu.pc);
     return false;
   }
