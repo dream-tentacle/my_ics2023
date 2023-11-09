40663a3e9026e56a5630ee9f848b92d07452769e
tracer-ics2023 <tracer@njuics.org>
2023-11-09 14:33:22 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dt-ubuntu 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:33:22 up  1:36,  1 user,  load average: 0.80, 0.65, 0.66

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,31 +5,64 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  size_t re = 0;
-  while (*s != '\0') {
-    re++;
-    s++;
-  }
-  return re;
+  size_t i;
+  for (i = 0; s[i] != '\0'; i++)
+    ;
+  return i;
 }
 
-char *strcpy(char *dst, const char *src) { panic("Not implemented"); }
+char *strcpy(char *dst, const char *src) {
+  size_t i;
+
+  for (i = 0; src[i] != '\0'; i++)
+    dst[i] = src[i];
+  dst[i] = '\0';
+  return dst;
+}
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  size_t i;
+
+  for (i = 0; i < n && src[i] != '\0'; i++)
+    dst[i] = src[i];
+  for (; i < n; i++)
+    dst[i] = '\0';
+
+  return dst;
 }
 
-char *strcat(char *dst, const char *src) { panic("Not implemented"); }
+char *strcat(char *dst, const char *src) {
+  size_t dest_len = strlen(dst);
+  size_t i;
+
+  for (i = 0; src[i] != '\0'; i++)
+    dst[dest_len + i] = src[i];
+  dst[dest_len + i] = '\0';
+
+  return dst;
+}
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  size_t i;
+  for (i = 0; s1[i] == s2[i] && s1[i] != '\0'; i++)
+    ;
+  return s1[i] - s2[i];
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  size_t i;
+  for (i = 0; i < n && s1[i] == s2[i] && s1[i] != '\0'; i++)
+    ;
+  return s1[i] - s2[i];
 }
 
-void *memset(void *s, int c, size_t n) { panic("Not implemented"); }
+void *memset(void *s, int c, size_t n) {
+  size_t i;
+  char *ch = s;
+  for (i = 0; i < n; i++)
+    ch[i] = c;
+  return s;
+}
 
 void *memmove(void *dst, const void *src, size_t n) {
   panic("Not implemented");
