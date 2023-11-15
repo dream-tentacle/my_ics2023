d16a84b69648a37b07f095812113eafe333d095e
tracer-ics2023 <tracer@njuics.org>
2023-11-15 16:37:59 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:37:59 up  2:03,  1 user,  load average: 0.65, 0.74, 0.73

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -1,10 +1,11 @@
 #include <am.h>
 #include <nemu.h>
-
+#include <stdio.h>
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t tmp = inl(KBD_ADDR);
+  printf("%u", tmp);
   kbd->keydown = (tmp & KEYDOWN_MASK) != 0;
   kbd->keycode = tmp & ~KEYDOWN_MASK;
 }
