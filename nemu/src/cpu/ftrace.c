efeb1db720ef51eac91ba100120a2b4d8e9032ec
tracer-ics2023 <tracer@njuics.org>
2023-11-25 14:06:01 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:06:01 up  5:02,  1 user,  load average: 0.75, 0.70, 0.68

--- nemu/src/cpu/ftrace.c
+++ nemu/src/cpu/ftrace.c
@@ -2,18 +2,41 @@
 #include <cpu/ftrace.h>
 // #include <elf.h>
 #include <stdlib.h>
-// void elf_process(char *file_name) {
-//   FILE *fp = fopen(file_name, "r");
-//   Assert(fp, "Can not open '%s'", file_name);
 
-//   fclose(fp);
-// }
-
-static funct_info funct_table[10] = {{"_trm_init", 0x80000108, 32},
-                                     {"check", 0x80000010, 24},
-                                     {"_start", 0x80000000, 0},
-                                     {"main", 0x80000028, 212},
-                                     {"halt", 0x800000fc, 12}};
+/*
+  21: 80000254    32 FUNC    GLOBAL DEFAULT    1 _trm_init
+    22: 80009000     0 NOTYPE  GLOBAL DEFAULT    5 _stack_pointer
+    23: 80000274     0 NOTYPE  GLOBAL DEFAULT    1 _etext
+    24: 80000000     0 NOTYPE  GLOBAL DEFAULT  ABS _pmem_start
+    25: 80000294     0 NOTYPE  GLOBAL DEFAULT    5 _bss_start
+    26: 80000275     0 NOTYPE  GLOBAL DEFAULT    2 edata
+    27: 80000294     4 OBJECT  GLOBAL DEFAULT    5 lvl
+    28: 80009000     0 NOTYPE  GLOBAL DEFAULT    5 _heap_start
+    29: 80001000     0 NOTYPE  GLOBAL DEFAULT    5 _stack_top
+    30: 80000108   168 FUNC    GLOBAL DEFAULT    1 f3
+    31: 80009000     0 NOTYPE  GLOBAL DEFAULT    5 end
+    32: 800001b0    24 FUNC    GLOBAL DEFAULT    1 check
+    33: 80000274     0 NOTYPE  GLOBAL DEFAULT    1 etext
+    34: 800000a4   100 FUNC    GLOBAL DEFAULT    1 f2
+    35: 80000000     0 FUNC    GLOBAL DEFAULT    1 _start
+    36: 00000000     0 NOTYPE  GLOBAL DEFAULT  ABS _entry_offset
+    37: 800001c8   128 FUNC    GLOBAL DEFAULT    1 main
+    38: 80000010    76 FUNC    GLOBAL DEFAULT    1 f0
+    39: 80000275     0 NOTYPE  GLOBAL DEFAULT    2 _data
+    40: 80000278    12 OBJECT  GLOBAL DEFAULT    3 ans
+    41: 80000284    16 OBJECT  GLOBAL DEFAULT    4 func
+    42: 8000005c    72 FUNC    GLOBAL DEFAULT    1 f1
+    43: 80009000     0 NOTYPE  GLOBAL DEFAULT    5 _end
+    44: 80000248    12 FUNC    GLOBAL DEFAULT    1 halt
+    45: 80000298     4 OBJECT  GLOBAL DEFAULT    5 rec
+    */
+// 只需要FUNC类型的
+static funct_info funct_table[10] = {
+    {"f0", 0x80000010, 76},    {"f1", 0x8000005c, 72},
+    {"f2", 0x800000a4, 100},   {"f3", 0x80000108, 168},
+    {"check", 0x800001b0, 24}, {"main", 0x800001c8, 128},
+    {"halt", 0x80000248, 12},  {"_trm_init", 0x80000254, 32},
+    {"_start", 0x80000000, 0}};
 
 static jmp_log *jmp_head, *jmp_last;
 static int funct_layer = 0;  // 记录函数嵌套层数
