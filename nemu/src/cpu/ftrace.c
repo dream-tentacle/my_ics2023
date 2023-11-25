9c637f05465b08f18918161520ea9cc2be3da5c1
tracer-ics2023 <tracer@njuics.org>
2023-11-25 14:20:16 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:20:16 up  5:16,  1 user,  load average: 0.82, 0.76, 0.70

--- nemu/src/cpu/ftrace.c
+++ nemu/src/cpu/ftrace.c
@@ -4,37 +4,34 @@
 #include <stdlib.h>
 
 /*
-   10: 80000028     0 NOTYPE  LOCAL  DEFAULT    1 $x
-    11: 80000078     0 NOTYPE  LOCAL  DEFAULT    1 $x
-    12: 00000000     0 FILE    LOCAL  DEFAULT  ABS trm.c
-    13: 8000011c     0 NOTYPE  LOCAL  DEFAULT    1 $x
-    14: 80000128     0 NOTYPE  LOCAL  DEFAULT    1 $x
-    15: 80000148     1 OBJECT  LOCAL  DEFAULT    2 mainargs
-    16: 80000128    32 FUNC    GLOBAL DEFAULT    1 _trm_init
-    17: 80009000     0 NOTYPE  GLOBAL DEFAULT    3 _stack_pointer
-    18: 80000148     0 NOTYPE  GLOBAL DEFAULT    1 _etext
-    19: 80000000     0 NOTYPE  GLOBAL DEFAULT  ABS _pmem_start
-    20: 8000019c     0 NOTYPE  GLOBAL DEFAULT    3 _bss_start
-    21: 80000149     0 NOTYPE  GLOBAL DEFAULT    2 edata
-    22: 80009000     0 NOTYPE  GLOBAL DEFAULT    3 _heap_start
-    23: 80001000     0 NOTYPE  GLOBAL DEFAULT    3 _stack_top
-    24: 80009000     0 NOTYPE  GLOBAL DEFAULT    3 end
-    25: 80000010    24 FUNC    GLOBAL DEFAULT    1 check
-    26: 80000148     0 NOTYPE  GLOBAL DEFAULT    1 etext
-    27: 80000000     0 FUNC    GLOBAL DEFAULT    1 _start
-    28: 00000000     0 NOTYPE  GLOBAL DEFAULT  ABS _entry_offset
-    29: 80000078   164 FUNC    GLOBAL DEFAULT    1 main
-    30: 80000149     0 NOTYPE  GLOBAL DEFAULT    2 _data
-    31: 80009000     0 NOTYPE  GLOBAL DEFAULT    3 _end
-    32: 8000011c    12 FUNC    GLOBAL DEFAULT    1 halt
-    33: 80000028    80 FUNC    GLOBAL DEFAULT    1 bubble_sort
+    17: 800003b4    32 FUNC    GLOBAL DEFAULT    1 _trm_init
+    18: 80009000     0 NOTYPE  GLOBAL DEFAULT    3 _stack_pointer
+    19: 800003d4     0 NOTYPE  GLOBAL DEFAULT    1 _etext
+    20: 80000000     0 NOTYPE  GLOBAL DEFAULT  ABS _pmem_start
+    21: 80000428     0 NOTYPE  GLOBAL DEFAULT    3 _bss_start
+    22: 800003d5     0 NOTYPE  GLOBAL DEFAULT    2 edata
+    23: 80009000     0 NOTYPE  GLOBAL DEFAULT    3 _heap_start
+    24: 80001000     0 NOTYPE  GLOBAL DEFAULT    3 _stack_top
+    25: 80000028   200 FUNC    GLOBAL DEFAULT    1 partition
+    26: 80009000     0 NOTYPE  GLOBAL DEFAULT    3 end
+    27: 80000010    24 FUNC    GLOBAL DEFAULT    1 check
+    28: 800003d4     0 NOTYPE  GLOBAL DEFAULT    1 etext
+    29: 80000000     0 FUNC    GLOBAL DEFAULT    1 _start
+    30: 00000000     0 NOTYPE  GLOBAL DEFAULT  ABS _entry_offset
+    31: 800002e8   192 FUNC    GLOBAL DEFAULT    1 main
+    32: 800003d5     0 NOTYPE  GLOBAL DEFAULT    2 _data
+    33: 80009000     0 NOTYPE  GLOBAL DEFAULT    3 _end
+    34: 800003a8    12 FUNC    GLOBAL DEFAULT    1 halt
+    35: 800003d8    80 OBJECT  GLOBAL DEFAULT    3 a
+    36: 800000f0   504 FUNC    GLOBAL DEFAULT    1 quick_sort
     */
 // 只需要FUNC类型的
 // name, addr, size
 static funct_info funct_table[10] = {
-    {"_trm_init", 0x80000128, 32}, {"check", 0x80000010, 24},
-    {"_start", 0x80000000, 0},     {"main", 0x80000078, 164},
-    {"halt", 0x8000011c, 12},      {"bubble_sort", 0x80000028, 80}};
+    {"_trm_init", 0x800003b4, 32},  {"partition", 0x80000028, 200},
+    {"check", 0x80000010, 24},      {"_start", 0x80000000, 0},
+    {"main", 0x800002e8, 192},      {"halt", 0x800003a8, 12},
+    {"quick_sort", 0x800000f0, 504}};
 
 static jmp_log *jmp_head, *jmp_last;
 static int funct_layer = 0;  // 记录函数嵌套层数
