7217e4a5271d3a6befe139367dc2453268bc61e9
tracer-ics2023 <tracer@njuics.org>
2023-11-26 17:01:17 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:01:17 up  1:26,  1 user,  load average: 0.88, 0.70, 0.72

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -211,7 +211,7 @@ static int decode_exec(Decode* s) {
           Assert(src2 != 0, "div by zero in riscv32/inst.c");
           R(rd) = src1 % src2);
 
-  INSTPAT("0000000000000000000000000111 0011", ecall, N, );
+  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall, N, );
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak, N,
           NEMUTRAP(s->pc, R(10)));  // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv, N, INV(s->pc));
