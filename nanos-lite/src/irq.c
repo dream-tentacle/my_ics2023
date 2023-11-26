4b9fffb47e33fd97d7db03a3531fcb52f8485df4
tracer-ics2023 <tracer@njuics.org>
2023-11-26 21:13:14 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:13:14 up  5:38,  1 user,  load average: 2.09, 1.46, 1.43

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,8 +1,12 @@
 #include <common.h>
 
-static Context* do_event(Event e, Context* c) {
+static Context *do_event(Event e, Context *c) {
   switch (e.event) {
-    default: panic("Unhandled event ID = %d", e.event);
+  case EVENT_YIELD:
+    Log("EVENT_YIELD");
+    break;
+  default:
+    panic("Unhandled event ID = %d", e.event);
   }
 
   return c;
