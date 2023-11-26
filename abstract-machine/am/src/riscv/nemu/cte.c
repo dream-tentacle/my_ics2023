327e2c0ed07230419eeb20ec861d1b66da6bf072
221240060-Xiong Juncheng <221240060@smail.nju.edu.cn>
2023-11-26 20:42:08 +0800

    NJU-ProjectN/nanos-lite ics2021 initialized
    NJU-ProjectN/nanos-lite 2a141760e31be246a7316942293a97873925bc2f Makefile: use header files in newlib-navy

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -16,7 +16,7 @@ Context *__am_irq_handle(Context *c) {
     Event ev = {0};
     switch (c->mcause) {
       default:
-        if (1) {
+        if (c->GPR1 == -1) {
           ev.event = EVENT_YIELD;
         } else {
           ev.event = EVENT_ERROR;
