bdd8a321021fa894a67e68dae385798669e3db30
tracer-ics2023 <tracer@njuics.org>
2023-11-26 18:00:25 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:00:25 up  2:25,  1 user,  load average: 1.06, 0.93, 0.87

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -30,7 +30,7 @@ bool cte_init(Context *(*handler)(Event, Context *)) {
 
   // register event handler
   user_handler = handler;
-  printf("__am_asm_trap = %x\n", __am_asm_trap);
+  printf("__am_asm_trap = %ld\n", __am_asm_trap);
 
   return true;
 }
