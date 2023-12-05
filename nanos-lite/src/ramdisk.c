682b632e0017f86085c0fa93ec8f884a44ff3f0a
tracer-ics2023 <tracer@njuics.org>
2023-12-05 10:32:30 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:32:30 up  1:45,  1 user,  load average: 0.76, 0.76, 0.63

--- nanos-lite/src/ramdisk.c
+++ nanos-lite/src/ramdisk.c
@@ -12,7 +12,7 @@ extern uint8_t ramdisk_end;
 /* read `len' bytes starting from `offset' of ramdisk into `buf' */
 size_t ramdisk_read(void *buf, size_t offset, size_t len) {
   assert(offset + len <= RAMDISK_SIZE);
-  printf("%d", &ramdisk_start);
+  printf("\n%u\n", &ramdisk_start);
   // memcpy(buf, (&ramdisk_start) + offset, len);
   assert(0);
   return len;
