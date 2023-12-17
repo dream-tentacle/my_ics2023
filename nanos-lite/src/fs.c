878051ae8f323a30b7c2f873451613a5b3801b9e
tracer-ics2023 <tracer@njuics.org>
2023-12-17 15:29:09 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:29:09 up  5:12,  1 user,  load average: 0.90, 0.65, 0.54

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -64,7 +64,10 @@ int fs_write(int fd, void *buf, size_t count) {
 }
 int fs_read(int fd, void *buf, size_t count) {
   if (fd != 0) {
-    assert(file_table[fd].open_offset + count <= file_table[fd].size);
+    if (file_table[fd].open_offset + count <= file_table[fd].size)
+      panic("fs_read: file_table[fd].open_offset + count = %d, "
+            "file_table[fd].size = %d\n",
+            file_table[fd].open_offset + count, file_table[fd].size);
     ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset,
                  count);
     file_table[fd].open_offset += count;
