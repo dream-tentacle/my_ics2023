bb0d57b2aab2d7706b751fd4a694fb9a0da20e9e
tracer-ics2023 <tracer@njuics.org>
2023-12-17 15:29:24 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:29:24 up  5:13,  1 user,  load average: 0.92, 0.66, 0.55

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -64,7 +64,7 @@ int fs_write(int fd, void *buf, size_t count) {
 }
 int fs_read(int fd, void *buf, size_t count) {
   if (fd != 0) {
-    if (file_table[fd].open_offset + count <= file_table[fd].size)
+    if (file_table[fd].open_offset + count > file_table[fd].size)
       panic("fs_read: file_table[fd].open_offset + count = %d, "
             "file_table[fd].size = %d\n",
             file_table[fd].open_offset + count, file_table[fd].size);
