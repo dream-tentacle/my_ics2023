783d648588e5869df913b2d2a53d6dc7a13ba91f
tracer-ics2023 <tracer@njuics.org>
2023-12-17 15:18:05 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:18:05 up  5:01,  1 user,  load average: 0.39, 0.48, 0.54

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -49,7 +49,8 @@ int fs_open(const char *path, int flags, int mode) {
 int fs_write(int fd, void *buf, size_t count) {
   if (fd == 1 || fd == 2) {
     char *str = (char *)buf;
-    for (int i = 0; i < 10; i++) {
+    printf("%s\n", str);
+    for (int i = 0; i < count; i++) {
       putch(str[i]);
     }
     return count;
