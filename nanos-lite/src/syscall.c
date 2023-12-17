4840436e5272ee69e70e0291a4b503499c8c30aa
tracer-ics2023 <tracer@njuics.org>
2023-12-17 10:25:40 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:25:40 up 9 min,  1 user,  load average: 0.60, 0.44, 0.22

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -28,7 +28,7 @@ void do_syscall(Context *c) {
     sys_exit(c->GPR2);
     break;
   case SYS_write:
-    printf("sys_write, return %d\n", c->GPRx);
+    printf("sys_write, return %d\n", c->GPR4);
     c->GPRx = sys_write(c->GPR2, (void *)c->GPR3, c->GPR4);
     while (1)
       ;
