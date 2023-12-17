3d9424ff3b00121708ada1adad29f2b4d157e483
tracer-ics2023 <tracer@njuics.org>
2023-12-17 14:17:28 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:17:28 up  4:01,  1 user,  load average: 0.79, 0.66, 0.53

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -18,12 +18,12 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   // 模数检查
   assert((*(uint32_t *)elf_ehdr.e_ident == 0x464c457f));
   Elf_Phdr elf_phdr[elf_ehdr.e_phnum];
+  printf("elf_ehdr.e_phnum = %d\n", elf_ehdr.e_phnum);
   for (int i = 0; i < elf_ehdr.e_phnum; i++) {
     if (elf_phdr[i].p_type == 1) {
       // 从ramdisk中读取数据
       fs_lseek(fd, elf_phdr[i].p_offset, SEEK_SET);
       fs_read(fd, (void *)elf_phdr[i].p_vaddr, elf_phdr[i].p_memsz);
-      printf("%x %x", elf_phdr[i].p_vaddr, elf_phdr[i].p_memsz);
       // 将未初始化的数据置为0
       memset((void *)(elf_phdr[i].p_vaddr + elf_phdr[i].p_filesz), 0,
              elf_phdr[i].p_memsz - elf_phdr[i].p_filesz);
