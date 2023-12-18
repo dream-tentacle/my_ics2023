b9e3665a8a8fc496beee07071e81bc337c8931f2
tracer-ics2023 <tracer@njuics.org>
2023-12-18 20:58:43 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:58:43 up  1:42,  1 user,  load average: 0.72, 0.65, 0.48

--- navy-apps/tests/time-test/time-test.c
+++ navy-apps/tests/time-test/time-test.c
@@ -3,15 +3,16 @@
 #include <sys/time.h>
 
 struct timeval tv, last;
+struct timezone tz;
 int main() {
   int a = 0;
-  gettimeofday(&tv, 0);
+  gettimeofday(&tv, &tz);
   last.tv_sec = tv.tv_sec;
   last.tv_usec = tv.tv_usec;
   while (1) {
     a++;
     while (1) {
-      gettimeofday(&tv, 0);
+      gettimeofday(&tv, &tz);
       if (tv.tv_usec - last.tv_usec >= 500) {
         last.tv_sec = tv.tv_sec;
         last.tv_usec = tv.tv_usec;
