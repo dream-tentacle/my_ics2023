f5291acac7ccefded6399448cba87d76b3defaf8
tracer-ics2023 <tracer@njuics.org>
2023-12-18 19:29:23 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:29:23 up 13 min,  1 user,  load average: 0.91, 0.74, 0.41

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -19,7 +19,7 @@ int sys_lseek(int fd, size_t offset, int whence) {
 void sys_brk(int addr) {}
 #define STRACE
 #ifdef STRACE
-#define strace printf
+#define strace(s, ...) printf("| " s " |\n", ##__VA_ARGS__)
 #else
 #define strace(...)
 #endif
@@ -32,20 +32,19 @@ void do_syscall(Context *c) {
   case SYS_yield:
     sys_yield();
     c->GPRx = 0;
-    strace("sys_yield, return 0\n");
+    strace("sys_yield, return 0");
     break;
   case SYS_exit:
-    strace("sys_exit, return %d\n", c->GPR2);
+    strace("sys_exit, return %d", c->GPR2);
     sys_exit(c->GPR2);
     break;
   case SYS_write:
-    strace("sys_write, write %s, return %d\n", file_table[c->GPR2].name,
-           c->GPRx);
+    strace("sys_write, write %s, return %d", file_table[c->GPR2].name, c->GPRx);
     c->GPRx = sys_write(c->GPR2, (void *)c->GPR3, c->GPR4);
     strace("return %d\n", c->GPRx);
     break;
   case SYS_read:
-    strace("sys_read, read from %s, return %d\n", file_table[c->GPR2].name,
+    strace("sys_read, read from %s, return %d", file_table[c->GPR2].name,
            c->GPRx);
     c->GPRx = sys_read(c->GPR2, (void *)c->GPR3, c->GPR4);
     strace("return %d\n", c->GPRx);
@@ -53,22 +52,22 @@ void do_syscall(Context *c) {
   case SYS_brk:
     sys_brk(c->GPR2);
     c->GPRx = 0; // 以后PA4动态申请内存时还会调整
-    strace("sys_brk, return 0\n");
+    strace("sys_brk, return 0");
     break;
   case SYS_close:
-    strace("sys_close, close %s, return 0\n", file_table[c->GPR2].name);
+    strace("sys_close, close %s, return 0", file_table[c->GPR2].name);
     c->GPRx = 0;
     break;
   case SYS_lseek:
     fd = c->GPR2;
     c->GPRx = sys_lseek(c->GPR2, c->GPR3, c->GPR4);
-    strace("sys_lseek, lseek %s, offset set to %d, return %d\n",
+    strace("sys_lseek, lseek %s, offset set to %d, return %d",
            file_table[fd].name, file_table[fd].open_offset, c->GPRx);
     break;
   case SYS_open:
     char *path = (char *)c->GPR2;
     c->GPRx = sys_open((const char *)c->GPR2, c->GPR3, c->GPR4);
-    strace("sys_open, open file %s, return %d\n", path, c->GPRx);
+    strace("sys_open, open file %s, return %d", path, c->GPRx);
     break;
   default:
     panic("Unhandled syscall ID = %d", a[0]);
