792c4f25508487c50bf1374ffc965b6206ab43ed
tracer-ics2023 <tracer@njuics.org>
2023-12-19 10:17:19 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:17:19 up 7 min,  1 user,  load average: 0.63, 0.49, 0.23

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -18,7 +18,17 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
   return len;
 }
 
-size_t events_read(void *buf, size_t offset, size_t len) { return 0; }
+size_t events_read(void *buf, size_t offset, size_t len) {
+  AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
+  if (ev.keycode == AM_KEY_NONE) {
+    return 0;
+  }
+  if (ev.keydown) {
+    return sprintf(buf, "kd %s\n", keyname[ev.keycode]);
+  } else {
+    return sprintf(buf, "ku %s\n", keyname[ev.keycode]);
+  }
+}
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) { return 0; }
 
