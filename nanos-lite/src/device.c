908a9b87db9774403c7a745ab0b39812d6415413
tracer-ics2023 <tracer@njuics.org>
2023-12-19 21:00:24 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:00:24 up 10:50,  1 user,  load average: 1.10, 0.80, 0.69

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -35,18 +35,14 @@ size_t dispinfo_read(char *buf, size_t offset, size_t len) {
   return sprintf(buf, "WIDTH=%d,HEIGHT=%d\n", gpu_config.width,
                  gpu_config.height);
 }
-static int count = 0;
 size_t fb_write(const void *buf, size_t offset, size_t len) {
   AM_GPU_CONFIG_T gpu_config = io_read(AM_GPU_CONFIG);
   int w = offset % gpu_config.width;
   int h = offset / gpu_config.width;
   void *tmp = (void *)buf;
   io_write(AM_GPU_FBDRAW, w, h, tmp, len / sizeof(uint32_t), 1, false);
-  count++;
-  if (count == 75) {
-    while (1)
-      ;
-  }
+  printf("w = %d, h = %d, len = %d\n", w, h, len);
+
   return len;
 }
 
