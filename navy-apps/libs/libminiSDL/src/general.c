c80af44070c2c100d280366aaaaa54cf077f506e
tracer-ics2023 <tracer@njuics.org>
2023-12-20 13:29:43 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:29:43 up 38 min,  1 user,  load average: 0.58, 0.57, 0.50

--- navy-apps/libs/libminiSDL/src/general.c
+++ navy-apps/libs/libminiSDL/src/general.c
@@ -1,7 +1,7 @@
 #include <NDL.h>
 
 int SDL_Init(uint32_t flags) {
-  panic("SDL_Init() is not implemented!\n");
+  printf("SDL_Init() is not implemented!\n");
   return NDL_Init(flags);
 }
 
@@ -10,15 +10,15 @@ void SDL_Quit() { NDL_Quit(); }
 char *SDL_GetError() { return "Navy does not support SDL_GetError()"; }
 
 int SDL_SetError(const char *fmt, ...) {
-  panic("SDL_SetError() is not implemented!\n");
+  printf("SDL_SetError() is not implemented!\n");
   return -1;
 }
 
 int SDL_ShowCursor(int toggle) {
-  panic("SDL_ShowCursor() is not implemented!\n");
+  printf("SDL_ShowCursor() is not implemented!\n");
   return 0;
 }
 
 void SDL_WM_SetCaption(const char *title, const char *icon) {
-  panic("SDL_WM_SetCaption() is not implemented!\n");
+  printf("SDL_WM_SetCaption() is not implemented!\n");
 }
--- navy-apps/libs/libminiSDL/src/timer.c
+++ navy-apps/libs/libminiSDL/src/timer.c
@@ -4,18 +4,18 @@
 
 SDL_TimerID SDL_AddTimer(uint32_t interval, SDL_NewTimerCallback callback,
                          void *param) {
-  panic("SDL_AddTimer() is not implemented!\n");
+  printf("SDL_AddTimer() is not implemented!\n");
   return NULL;
 }
 
 int SDL_RemoveTimer(SDL_TimerID id) {
-  panic("SDL_RemoveTimer() is not implemented!\n");
+  printf("SDL_RemoveTimer() is not implemented!\n");
   return 1;
 }
 
 uint32_t SDL_GetTicks() {
-  panic("SDL_GetTicks() is not implemented!\n");
+  printf("SDL_GetTicks() is not implemented!\n");
   return 0;
 }
 
-void SDL_Delay(uint32_t ms) { panic("SDL_Delay() is not implemented!\n"); }
+void SDL_Delay(uint32_t ms) { printf("SDL_Delay() is not implemented!\n"); }
