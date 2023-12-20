35c1f334ee4d2adc8a3bdb5c43bbdb6f94d7867c
tracer-ics2023 <tracer@njuics.org>
2023-12-20 15:01:10 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:01:10 up  1:00,  1 user,  load average: 0.61, 0.72, 0.78

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -25,14 +25,14 @@ int SDL_WaitEvent(SDL_Event *event) {
   }
   if (buf[0] == 'k' && buf[1] == 'd') {
     event->type = SDL_KEYDOWN;
-    char key[64];
-    sscanf(buf, "kd %s\n", key);
-    for (int i = 0; i < 83; i++) {
-      if (strcmp(key, keyname[i]) == 0) {
-        event->key.keysym.sym = i;
-        break;
-      }
-    }
+    // char key[64];
+    // sscanf(buf, "kd %s\n", key);
+    // for (int i = 0; i < 83; i++) {
+    //   if (strcmp(key, keyname[i]) == 0) {
+    //     event->key.keysym.sym = i;
+    //     break;
+    //   }
+    // }
   }
   if (buf[0] == 'k' && buf[1] == 'u') {
     event->type = SDL_KEYUP;
