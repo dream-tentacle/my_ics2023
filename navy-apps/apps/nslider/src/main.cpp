93a2037182dccd311f707f36759bd0fe14af1b74
tracer-ics2023 <tracer@njuics.org>
2023-12-20 14:22:48 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:22:48 up 22 min,  1 user,  load average: 0.42, 0.54, 0.44

--- navy-apps/apps/nslider/src/main.cpp
+++ navy-apps/apps/nslider/src/main.cpp
@@ -17,7 +17,7 @@ const int N = 10;
 const char *path = "/share/slides/slides-%d.bmp";
 
 static SDL_Surface *slide = NULL;
-static int cur = 0;
+static int cur = 1;
 
 void render() {
   if (slide) {
@@ -31,16 +31,20 @@ void render() {
 }
 
 void prev(int rep) {
-  if (rep == 0) rep = 1;
+  if (rep == 0)
+    rep = 1;
   cur -= rep;
-  if (cur < 0) cur = 0;
+  if (cur < 0)
+    cur = 0;
   render();
 }
 
 void next(int rep) {
-  if (rep == 0) rep = 1;
+  if (rep == 0)
+    rep = 1;
   cur += rep;
-  if (cur >= N) cur = N - 1;
+  if (cur >= N)
+    cur = N - 1;
   render();
 }
 
@@ -57,28 +61,57 @@ int main() {
     SDL_WaitEvent(&e);
 
     if (e.type == SDL_KEYDOWN) {
-      switch(e.key.keysym.sym) {
-        case SDLK_0: rep = rep * 10 + 0; break;
-        case SDLK_1: rep = rep * 10 + 1; break;
-        case SDLK_2: rep = rep * 10 + 2; break;
-        case SDLK_3: rep = rep * 10 + 3; break;
-        case SDLK_4: rep = rep * 10 + 4; break;
-        case SDLK_5: rep = rep * 10 + 5; break;
-        case SDLK_6: rep = rep * 10 + 6; break;
-        case SDLK_7: rep = rep * 10 + 7; break;
-        case SDLK_8: rep = rep * 10 + 8; break;
-        case SDLK_9: rep = rep * 10 + 9; break;
-        case SDLK_J:
-        case SDLK_DOWN: next(rep); rep = 0; g = 0; break;
-        case SDLK_K:
-        case SDLK_UP: prev(rep); rep = 0; g = 0; break;
-        case SDLK_G:
-          g ++;
-          if (g > 1) {
-            prev(100000);
-            rep = 0; g = 0;
-          }
-          break;
+      switch (e.key.keysym.sym) {
+      case SDLK_0:
+        rep = rep * 10 + 0;
+        break;
+      case SDLK_1:
+        rep = rep * 10 + 1;
+        break;
+      case SDLK_2:
+        rep = rep * 10 + 2;
+        break;
+      case SDLK_3:
+        rep = rep * 10 + 3;
+        break;
+      case SDLK_4:
+        rep = rep * 10 + 4;
+        break;
+      case SDLK_5:
+        rep = rep * 10 + 5;
+        break;
+      case SDLK_6:
+        rep = rep * 10 + 6;
+        break;
+      case SDLK_7:
+        rep = rep * 10 + 7;
+        break;
+      case SDLK_8:
+        rep = rep * 10 + 8;
+        break;
+      case SDLK_9:
+        rep = rep * 10 + 9;
+        break;
+      case SDLK_J:
+      case SDLK_DOWN:
+        next(rep);
+        rep = 0;
+        g = 0;
+        break;
+      case SDLK_K:
+      case SDLK_UP:
+        prev(rep);
+        rep = 0;
+        g = 0;
+        break;
+      case SDLK_G:
+        g++;
+        if (g > 1) {
+          prev(100000);
+          rep = 0;
+          g = 0;
+        }
+        break;
       }
     }
   }
