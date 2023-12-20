1418195adbdd7b4a80618ce11a21c3ce7646aa03
tracer-ics2023 <tracer@njuics.org>
2023-12-20 15:23:17 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:23:17 up  1:23,  1 user,  load average: 1.61, 1.11, 0.96

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -166,6 +166,7 @@ static void draw_text_row(char *s, int r) {
 }
 
 static void display_menu(int n) {
+  printf("123");
   clear_display();
   SDL_Rect rect = {.x = screen->w - logo_sf->w, .y = 0};
   SDL_BlitSurface(logo_sf, NULL, screen, &rect);
