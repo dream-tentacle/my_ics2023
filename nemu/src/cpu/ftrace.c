f369a6445d93450a2d641bb9447c7fc40bc5e6a2
tracer-ics2023 <tracer@njuics.org>
2023-12-21 19:29:04 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:29:04 up  6:26,  1 user,  load average: 1.19, 0.79, 0.82

--- nemu/src/cpu/ftrace.c
+++ nemu/src/cpu/ftrace.c
@@ -80,7 +80,6 @@ void call_funct(unsigned int addr, unsigned int pc) {
       jmp_head[funct_layer].layer = funct_layer;
       jmp_head[funct_layer].now_pc = pc;
       funct_layer++;
-      printf("%s %d\n", now->name, funct_layer);
       return;
     }
   }
