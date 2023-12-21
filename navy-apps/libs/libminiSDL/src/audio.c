38a378585e22abb99d970d350bf1ad26a44bb794
tracer-ics2023 <tracer@njuics.org>
2023-12-21 19:53:40 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:53:40 up  6:51,  1 user,  load average: 1.87, 1.59, 1.24

--- navy-apps/libs/libminiSDL/src/audio.c
+++ navy-apps/libs/libminiSDL/src/audio.c
@@ -1,31 +1,21 @@
 #include <NDL.h>
 #include <SDL.h>
 
-int SDL_OpenAudio(SDL_AudioSpec *desired, SDL_AudioSpec *obtained) {
-  printf("SDL_OpenAudio() is not implemented!\n");
-  return 0;
-}
+int SDL_OpenAudio(SDL_AudioSpec *desired, SDL_AudioSpec *obtained) { return 0; }
 
-void SDL_CloseAudio() { printf("SDL_CloseAudio() is not implemented!\n"); }
+void SDL_CloseAudio() {}
 
-void SDL_PauseAudio(int pause_on) {
-  printf("SDL_PauseAudio() is not implemented!\n");
-}
+void SDL_PauseAudio(int pause_on) {}
 
-void SDL_MixAudio(uint8_t *dst, uint8_t *src, uint32_t len, int volume) {
-  printf("SDL_MixAudio() is not implemented!\n");
-}
+void SDL_MixAudio(uint8_t *dst, uint8_t *src, uint32_t len, int volume) {}
 
 SDL_AudioSpec *SDL_LoadWAV(const char *file, SDL_AudioSpec *spec,
                            uint8_t **audio_buf, uint32_t *audio_len) {
-  printf("SDL_LoadWAV() is not implemented!\n");
   return NULL;
 }
 
-void SDL_FreeWAV(uint8_t *audio_buf) {
-  printf("SDL_FreeWAV() is not implemented!\n");
-}
+void SDL_FreeWAV(uint8_t *audio_buf) {}
 
-void SDL_LockAudio() { printf("SDL_LockAudio() is not implemented!\n"); }
+void SDL_LockAudio() {}
 
-void SDL_UnlockAudio() { printf("SDL_UnlockAudio() is not implemented!\n"); }
+void SDL_UnlockAudio() {}
