ae969151edc3fe66f51097359d6aa74ec9ef05e2
tracer-ics2023 <tracer@njuics.org>
2023-12-21 22:00:46 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:00:46 up 20 min,  1 user,  load average: 1.40, 1.03, 0.72

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -21,7 +21,7 @@ void hello_fun(void *arg) {
 
 void init_proc() {
   switch_boot_pcb();
-  naive_uload(NULL, "/bin/171860575");
+  naive_uload(NULL, "/bin/pal");
   Log("Initializing processes...");
 
   // load program here
