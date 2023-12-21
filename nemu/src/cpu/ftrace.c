f27769c64b2195cf853ba3c38e6320d279590106
tracer-ics2023 <tracer@njuics.org>
2023-12-21 19:28:54 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:28:54 up  6:26,  1 user,  load average: 0.79, 0.69, 0.79

--- nemu/src/cpu/ftrace.c
+++ nemu/src/cpu/ftrace.c
@@ -71,19 +71,11 @@ void call_funct(unsigned int addr, unsigned int pc) {
          (pc < now->addr || pc > now->addr + now->size))) {
       // printf("now->addr: %x, addr: %x\n", now->addr, addr);
       // if (now->addr <= addr && now->addr + now->size > addr) {
-      last_pc[last_pc_cnt++] = pc;
-      if (last_pc_cnt >= 999) {
-        for (int i = 0; i < 999; i++) {
-          jmp_head[i].name = jmp_head[i + 1].name;
-          jmp_head[i].layer = jmp_head[i + 1].layer;
-          jmp_head[i].now_pc = jmp_head[i + 1].now_pc;
-        }
-        last_pc_cnt--;
-        jmp_head[999].name = now->name;
-        jmp_head[999].layer = funct_layer;
-        jmp_head[999].now_pc = pc;
-        return;
+      if (funct_layer >= 1000) {
+        funct_layer = 0;
+        last_pc_cnt = 0;
       }
+      last_pc[last_pc_cnt++] = pc;
       jmp_head[funct_layer].name = now->name;
       jmp_head[funct_layer].layer = funct_layer;
       jmp_head[funct_layer].now_pc = pc;
