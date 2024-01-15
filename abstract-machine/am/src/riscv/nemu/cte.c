ff60848271538049316cd2bac5b39f2cae188fab
tracer-ics2023 <tracer@njuics.org>
2024-01-15 20:16:06 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:16:06 up 40 min,  1 user,  load average: 0.86, 0.74, 0.55

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -35,7 +35,7 @@ Context *__am_irq_handle(Context *c) {
     c = user_handler(ev, c);
     assert(c != NULL);
   }
-  printf("mepc: %x\n", c->mepc);
+  printf("mepc: %h\n", c->mepc);
   return c;
 }
 
