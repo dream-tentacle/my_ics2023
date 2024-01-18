e72873bc85cd90e07897dab95c9b62bdfe8f65f4
tracer-ics2023 <tracer@njuics.org>
2024-01-18 20:22:54 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:22:54 up  1:20,  1 user,  load average: 0.70, 0.84, 0.60

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -12,7 +12,7 @@ void switch_boot_pcb() { current = &pcb_boot; }
 void hello_fun(void *arg) {
   int j = 1;
   while (1) {
-    Log("Hello World from Nanos-lite with arg '%c' for the %dth time!", arg, j);
+    Log("Hello World from Nanos-lite with arg '%s' for the %dth time!", arg, j);
     j++;
     yield();
   }
@@ -21,10 +21,10 @@ void context_kload(PCB *pcb, void *entry, void *arg) {
   pcb->cp = kcontext((Area){pcb->stack, pcb + 1}, entry, arg);
 }
 void init_proc() {
-  context_kload(&pcb[0], hello_fun, (void *)'a');
-  context_kload(&pcb[1], hello_fun, (void *)'b');
-  context_kload(&pcb[2], hello_fun, (void *)'c');
-  context_kload(&pcb[3], hello_fun, (void *)'d');
+  context_kload(&pcb[0], hello_fun, (void *)"a");
+  context_kload(&pcb[1], hello_fun, (void *)"b");
+  context_kload(&pcb[2], hello_fun, (void *)"c");
+  context_kload(&pcb[3], hello_fun, (void *)"d");
   switch_boot_pcb();
   // load program here
 }
