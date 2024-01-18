5fadfc677b07e3c071adb95f4cb6d67d8a8ff8b8
tracer-ics2023 <tracer@njuics.org>
2024-01-18 20:12:06 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:12:06 up  1:09,  1 user,  load average: 0.49, 0.52, 0.32

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -12,14 +12,18 @@ void switch_boot_pcb() { current = &pcb_boot; }
 void hello_fun(void *arg) {
   int j = 1;
   while (1) {
-    Log("Hello World from Nanos-lite with arg '%p' for the %dth time!",
+    Log("Hello World from Nanos-lite with arg '%c' for the %dth time!",
         (uintptr_t)arg, j);
     j++;
     yield();
   }
 }
-
+void context_kload(PCB *pcb, void *entry, void *arg) {
+  pcb->cp = kcontext((Area){pcb->stack, pcb + 1}, entry, arg);
+}
 void init_proc() {
+  context_kload(&pcb[0], hello_fun, (void *)'a');
+  context_kload(&pcb[1], hello_fun, (void *)'c');
   switch_boot_pcb();
   naive_uload(NULL, "/bin/nterm");
   Log("Initializing processes...");
@@ -27,4 +31,13 @@ void init_proc() {
   // load program here
 }
 
-Context *schedule(Context *prev) { return NULL; }
+Context *schedule(Context *prev) {
+  current->cp = prev;
+  for (int i = 0; i < MAX_NR_PROC; i++) {
+    if (current == &pcb[i]) {
+      current = &pcb[(i + 1) % MAX_NR_PROC];
+      break;
+    }
+  }
+  return current->cp;
+}
