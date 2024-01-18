d77986ce7253f2d8eb28d24b52bb0e9c25c30a84
tracer-ics2023 <tracer@njuics.org>
2024-01-18 20:16:05 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:16:05 up  1:13,  1 user,  load average: 0.93, 0.76, 0.46

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,9 +1,11 @@
 #include <common.h>
+#include <proc.h>
 extern void do_syscall(Context *c);
 
 static Context *do_event(Event e, Context *c) {
   switch (e.event) {
   case EVENT_YIELD:
+    c = schedule(c);
     break;
   case EVENT_SYSCALL:
     do_syscall(c);
