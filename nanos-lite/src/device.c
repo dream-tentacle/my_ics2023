641ca6cb37bb98566668fbd5a92cd2e3b34f2a94
tracer-ics2023 <tracer@njuics.org>
2024-01-18 22:14:07 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:14:07 up  1:26,  1 user,  load average: 1.15, 0.99, 1.05

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -12,6 +12,7 @@ static const char *keyname[256]
     __attribute__((used)) = {[AM_KEY_NONE] = "NONE", AM_KEYS(NAME)};
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
+  yield();
   for (int i = 0; i < len; i++) {
     putch(((char *)buf)[i]);
   }
