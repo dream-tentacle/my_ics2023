5b5200beafe6c5eda934a48ac038f5934039852d
tracer-ics2023 <tracer@njuics.org>
2024-01-19 16:18:56 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:18:56 up  1:47,  1 user,  load average: 0.29, 0.51, 0.66

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -42,13 +42,13 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[],
   for (int i = 0; i < argc; i++) {
     position -= strlen(argv[i]) + 1;
     *(char **)(sp + 4 * i) = position;
-    printf("*%p = %s\n", sp + 4 * i, position);
+    printf("*%p = %p\n", sp + 4 * i, position);
   }
   *(char *)(sp + 4 * argc) = 0;
   for (int i = 0; i < envc; i++) {
     position -= strlen(envp[i]) + 1;
     *(char **)(sp + 4 * (argc + 1 + i)) = position;
-    printf("*%p = %s\n", sp + 4 * (argc + 1 + i), position);
+    printf("*%p = %p\n", sp + 4 * (argc + 1 + i), position);
   }
   *(char *)(sp + 4 * (argc + 1 + envc)) = 0;
   sp -= 4;
