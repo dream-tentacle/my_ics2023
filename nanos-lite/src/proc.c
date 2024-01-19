b2eec949a0f611105c755c14e73027acb9db9b06
tracer-ics2023 <tracer@njuics.org>
2024-01-19 18:26:29 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:26:29 up  3:54,  1 user,  load average: 0.71, 0.74, 0.56

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -25,6 +25,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[],
   Log("User Context set entry = %p", entry);
   pcb->cp = ucontext(NULL, (Area){pcb->stack, pcb + 1}, (void *)entry);
   int sp = (int)new_page(8);
+  printf("sp: %x\n", sp);
   int argc = 0, envc = 0;
   while (argv[argc] != NULL) {
     sp -= strlen(argv[argc]) + 1;
@@ -64,7 +65,7 @@ void init_proc() {
   context_kload(&pcb[0], hello_fun, (void *)"kernel");
   char *argv[] = {"--skip", "123", NULL};
   char *envp[] = {"123111", NULL};
-  context_uload(&pcb[1], "/bin/pal", argv, envp);
+  context_uload(add_pcb(), "/bin/pal", argv, envp);
   switch_boot_pcb();
   // load program here
   // naive_uload(NULL, "/bin/pal");
