fec64c384ec9d8e47aaf16e29a58ef7510680011
tracer-ics2023 <tracer@njuics.org>
2024-01-19 21:56:33 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:56:33 up  7:24,  1 user,  load average: 1.15, 0.75, 0.88

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -34,7 +34,7 @@ static void sh_handle_cmd(const char *cmd) {
     printf("args[%d]=%s\n", i, args[i]);
   }
   args[i] = NULL;
-  char *v[] = {"pal", "--skip", "123", NULL};
+  char *const v[] = {"pal", "--skip", "123", NULL};
   printf("v[1]=%p\n", v[1]);
   execvp(filename, v);
 }
