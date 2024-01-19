2da4cffbdf62940e0ed91018d580f52c97f91855
tracer-ics2023 <tracer@njuics.org>
2024-01-19 15:52:40 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:52:40 up  1:20,  1 user,  load average: 1.25, 1.09, 0.88

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -10,6 +10,12 @@ void call_main(uintptr_t *args) {
   char **argv = (*args) + 4;
   char **envp = (*args) + 4 * argc + 8;
   environ = envp;
+  for (int i = 0; i < argc; i++) {
+    printf("argv[%d] = %s\n", i, argv[i]);
+  }
+  for (int i = 0; envp[i] != NULL; i++) {
+    printf("envp[%d] = %s\n", i, envp[i]);
+  }
   exit(main(argc, argv, envp));
   assert(0);
 }
