e74b20ca835dad4b40f9997abb1a3233eac3ef6d
tracer-ics2023 <tracer@njuics.org>
2024-01-20 11:34:02 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:34:02 up  2:29,  1 user,  load average: 0.39, 0.67, 0.53

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -71,7 +71,7 @@ void init_proc() {
   context_kload(&pcb[0], hello_fun, (void *)"kernel");
   char *argv[] = {NULL};
   char *envp[] = {NULL};
-  context_uload(&pcb[1], "/bin/nterm", argv, envp);
+  context_uload(&pcb[1], "/bin/busybox", argv, envp);
   switch_boot_pcb();
   yield();
   // load program here
