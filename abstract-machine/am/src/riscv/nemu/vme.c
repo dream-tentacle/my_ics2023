3400e2619c9cb24e6514d47d5ae388804838d31d
tracer-ics2023 <tracer@njuics.org>
2024-01-20 19:00:53 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:00:53 up  9:55,  1 user,  load average: 0.84, 0.71, 0.70

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -78,6 +78,11 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
   // 填写新的页表项，页表基地址为ptab
   PTE *pt = (PTE *)((ptab << 2) & ~0xfff);
   pt[vpn1] = (uintptr_t)pa >> 2 | 0x1;
+  if ((int)pa == 0x80001000) {
+    printf("va = %x, pa = %x\n", va, pa);
+    printf("ptab = %x, pt = %x\n", ptab, pt);
+    printf("pdir = %x\n", pdir);
+  }
 }
 
 Context *ucontext(AddrSpace *as, Area kstack, void *entry) {
