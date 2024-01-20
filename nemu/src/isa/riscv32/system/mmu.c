4b0cbd7276a7d92921f7d296b091b378b50fba12
tracer-ics2023 <tracer@njuics.org>
2024-01-20 19:09:47 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:09:47 up 10:04,  1 user,  load average: 1.33, 0.91, 0.75

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -24,11 +24,9 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   uint32_t pde = paddr_read((paddr_t)pde_p, 4);
   assert((pde & 1));                       // 页目录项有效
   uint32_t ptab = (pde << 2) & 0xfffff000; // 页表基地址
-  uint32_t pte = paddr_read(ptab + 4 * ((vaddr >> 12) & 0x3ff), 4);
-  printf("addr=%x\n", ptab | ((vaddr >> 12) & 0x3ff));
-  printf("pte: %x\n", pte);
+  long long pte_p = ptab | ((vaddr >> 12) & 0x3ff << 2);
+  uint32_t pte = paddr_read(pte_p, 4);
   assert((pte & 1)); // 页表项有效
-  printf("%x\n", (pte >> 10 << 12) | (vaddr & 0xfff));
   // 暂时设定页表项的物理地址等于虚拟地址
   assert(((pte >> 10 << 12) | (vaddr & 0xfff)) == vaddr);
   return (pte >> 10 << 12) | (vaddr & 0xfff); // 物理地址
