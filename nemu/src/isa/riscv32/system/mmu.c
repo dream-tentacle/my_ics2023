61bee7dadb0a800879b5e489bfa64d85027b2fbb
tracer-ics2023 <tracer@njuics.org>
2024-01-20 18:57:26 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:57:26 up  9:52,  1 user,  load average: 0.79, 0.71, 0.70

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -28,6 +28,7 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   uint32_t pte = paddr_read(ptab, (vaddr >> 12) & 0x3ff);
   printf("pte: %x\n", pte);
   assert((pte & 1)); // 页表项有效
+  printf("%x\n", (pte >> 10 << 12) | (vaddr & 0xfff));
   // 暂时设定页表项的物理地址等于虚拟地址
   assert(((pte >> 10 << 12) | (vaddr & 0xfff)) == vaddr);
   return (pte >> 10 << 12) | (vaddr & 0xfff); // 物理地址
