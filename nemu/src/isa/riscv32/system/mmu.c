bccbce805d39ee1b4760f8b469d4590306aa2371
tracer-ics2023 <tracer@njuics.org>
2024-01-20 18:18:03 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:18:03 up  9:13,  1 user,  load average: 1.90, 1.09, 0.71

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -18,10 +18,10 @@
 #include <memory/paddr.h>
 extern word_t satp;
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
-  uint32_t *pdir =
-      (uint32_t *)(long long)((satp & 0x3fffff) << 12); // 页目录基地址
+  unsigned *pdir =
+      (unsigned *)(long long)((satp & 0x3fffff) << 12); // 页目录基地址
   printf("&pdir[vaddr >> 22] = %x\n", vaddr >> 22);
-  uint32_t pde = pdir[vaddr >> 22]; // 页目录项
+  unsigned pde = pdir[vaddr >> 22]; // 页目录项
   assert(pde & 1);                  // 页目录项有效
   uint32_t *ptab = (uint32_t *)(long long)(pde >> 10 << 12); // 页表基地址
   uint32_t pte = ptab[(vaddr >> 12) & 0x3ff];                // 页表项
