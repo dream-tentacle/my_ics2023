d00e92c06754daac5ae0ce9c179d9aca7611dfb3
tracer-ics2023 <tracer@njuics.org>
2024-01-20 19:45:53 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:45:53 up 10:40,  1 user,  load average: 0.71, 0.88, 0.90

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -24,8 +24,10 @@ void context_kload(PCB *pcb, void *entry, void *arg) {
 void context_uload(PCB *pcb, const char *filename, char *const argv[],
                    char *const envp[]) {
   void *newpg = new_page(8) + 32 * 1024; // 用户栈物理地址
-  map(&pcb->as, pcb->as.area.end - 32 * 1024, newpg - 32 * 1024,
-      0); // 用户栈虚拟地址
+  for (int i = 1; i <= 8; i++) {
+    map(&pcb->as, pcb->as.area.end - i * 4 * 1024, newpg - i * 4 * 1024,
+        0); // 用户栈虚拟地址
+  }
   Log("new page area: %p,%p", newpg - 32 * 1024, newpg);
   void *sp = newpg;
   int argc = 0, envc = 0;
