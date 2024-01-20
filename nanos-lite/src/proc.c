f66b1bb221e3a1ce78b7344b8ed0ceb5699ef058
tracer-ics2023 <tracer@njuics.org>
2024-01-20 10:47:17 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:47:17 up  1:42,  1 user,  load average: 0.76, 0.59, 0.64

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -23,11 +23,12 @@ void context_kload(PCB *pcb, void *entry, void *arg) {
 }
 void context_uload(PCB *pcb, const char *filename, char *const argv[],
                    char *const envp[]) {
-  uintptr_t entry = loader(pcb, filename);
-  Log("User Context set entry = %p", entry);
   if (argv != 0)
     if (argv[1] != 0)
       printf("argv[1] = %d %d %d\n", argv[1][0], argv[1][1], argv[1][2]);
+  uintptr_t entry = loader(pcb, filename);
+  Log("User Context set entry = %p", entry);
+
   pcb->cp = ucontext(NULL, (Area){pcb->stack, pcb + 1}, (void *)entry);
   void *newpg = new_page(8);
   printf("low=%p,high=%p\n", newpg - 32 * 1024, newpg);
