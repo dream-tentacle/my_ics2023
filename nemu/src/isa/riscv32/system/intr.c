294516c125a64f2e28a71e4924ff443f2e767512
tracer-ics2023 <tracer@njuics.org>
2024-01-21 20:31:29 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:31:29 up 11:10,  1 user,  load average: 0.50, 0.65, 0.64

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -29,9 +29,9 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   return mtvep;
 }
 word_t isa_query_intr() {
-  if (cpu.INTR == true && mtvep != 0 && mstatus & 0x8) {
-    cpu.INTR = false;
-    return 0x80000007;
-  }
+  // if (cpu.INTR == true && mtvep != 0 && mstatus & 0x8) {
+  //   cpu.INTR = false;
+  //   return 0x80000007;
+  // }
   return INTR_EMPTY;
 }
