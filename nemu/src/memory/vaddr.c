f4c025669eddaa75726f3a4e1995db3fc083fc74
tracer-ics2023 <tracer@njuics.org>
2024-01-21 17:23:58 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:23:58 up  8:02,  1 user,  load average: 1.39, 0.97, 0.83

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -29,6 +29,9 @@ word_t vaddr_ifetch(vaddr_t addr, int len) {
 word_t vaddr_read(vaddr_t addr, int len) {
   if (isa_mmu_check(addr, len, MMU_LOAD) == MMU_DIRECT)
     return paddr_read(addr, len);
+  if (addr >> 4 == 0x40060a3 || addr >> 4 == 0x82248a3) {
+    printf("addr=%x, data=%x\n", addr, paddr_read(addr, len));
+  }
   // if ((satp << 12) == 0x821dd000)
   //   printf("read vaddr %x, ", addr);
   addr = isa_mmu_translate(addr, len, MEM_TYPE_READ);
