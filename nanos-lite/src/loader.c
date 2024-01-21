88e8e65967a1863a4f1c494418d2d471446f5772
tracer-ics2023 <tracer@njuics.org>
2024-01-21 17:52:53 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:52:53 up  8:31,  1 user,  load average: 1.20, 1.04, 0.84

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -59,12 +59,12 @@ uintptr_t loader(PCB *pcb, const char *filename) {
         }
         if (j == 0x40060000) {
           printf("map %p to %p\n", j, page);
-          char *x = 0x40060a08 + page - j;
-          for (; ((int)x) < (int)(0x40060af0 + page - j);
-               x = (char *)((int)x) + 1) {
-            printf("%x", (int)((char)*x) % 16);
-            if (((int)x) % 16 == 8)
+          int x = (int)(0x40060a08 + page - j);
+          for (; x < (int)(0x40060af0 + page - j); x++) {
+            printf("%x ", *(uint8_t *)x);
+            if (x % 16 == 15) {
               printf("\n");
+            }
           }
         }
       }
