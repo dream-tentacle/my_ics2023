f424185da09581ee738c33dad9cbd2df3452cb15
tracer-ics2023 <tracer@njuics.org>
2024-01-21 15:01:01 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:01:01 up  5:39,  1 user,  load average: 0.36, 0.37, 0.57

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -31,6 +31,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[],
     //        newpg - i * 4 * 1024);
   }
   memset(newpg - 32 * 1024, 0, 32 * 1024);
+  Log("123");
   Log("new page area: p %p,%p", newpg - 32 * 1024, newpg);
   Log("new page area: v %p,%p", pcb->as.area.end - 32 * 1024, pcb->as.area.end);
   void *sp = newpg;
