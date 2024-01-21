40a3523699d82caf316527a353978357b12a9352
tracer-ics2023 <tracer@njuics.org>
2024-01-21 15:27:55 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:27:55 up  6:06,  1 user,  load average: 0.50, 0.57, 0.63

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -64,30 +64,40 @@ void __am_switch(Context *c) {
     set_satp(c->pdir);
   }
 }
+// va
+// |VPN[1]|VPN[0]|Offset|
+// |  10  |  10  |  12  |
+// pa
+// |PPN[1]|PPN[0]|Offset|
+// |  10  |  10  |  12  |
+// PTE
+// |PPN[1]|PPN[0]| ... | V |
+// |  12  |  10  |    10   |
+#define VPN1(va) (((uintptr_t)(va) >> 22) & 0x3ff)
+#define VPN0(va) (((uintptr_t)(va) >> 12) & 0x3ff)
+#define OFFSET(va) ((uintptr_t)(va) & 0xfff)
+#define PPN1(pa) (((uintptr_t)(pa) >> 22) & 0x3ff)
+#define PPN0(pa) (((uintptr_t)(pa) >> 12) & 0x3ff)
+#define PTE_PPN1(pte) (((uintptr_t)(pte) >> 20) & 0x3ff)
+#define PTE_PPN0(pte) (((uintptr_t)(pte) >> 10) & 0x3ff)
+#define PTE_PPN(pte) (((uintptr_t)(pte) >> 10) & 0x3fffff)
 void map(AddrSpace *as, void *va, void *pa, int prot) {
   // 首先获取页目录项
-  PTE *pdir = (PTE *)as->ptr; // 页目录基地址
-  uint32_t vpn0 = (uintptr_t)va >> 22;
-  uint32_t vpn1 = ((uintptr_t)va >> 12) & 0x3ff;
-  PTE pde = pdir[vpn0];
-  if ((pde & 1) == 0) {
-    // 页表不存在
-    pde = (uintptr_t)pgalloc_usr(PGSIZE) >> 2 | 0x1;
-    pdir[vpn0] = pde;
+  PTE *page_dir = as->ptr;
+  PTE *page_dir_entry_p = &page_dir[VPN1(va)];
+  // 如果页目录项不存在，那么就分配一个页表
+  if (!(*page_dir_entry_p & 1)) {
+    PTE *page_table = (PTE *)(pgalloc_usr(PGSIZE));
+    memset(page_table, 0, PGSIZE);
+    *page_dir_entry_p = ((uintptr_t)page_table << 10) | 0x1;
   }
-  // 填写新的页表项，页表基地址为pde
-  PTE *pt = (PTE *)((pde << 2) & ~0xfff);
-  pt[vpn1] = (uintptr_t)pa >> 2 | 0x1;
-  if (as->ptr != kas.ptr) {
-    printf("map %p to %p, pde = %p, pt = %p\n", va, pa, pde, pt);
-  }
-  if ((int)pde == 0x20870801) {
-    printf("[[map %p to %p, &pde=%p,pde=%p,&pt=%p,pt=%p]]\n", va, pa, &pde, pde,
-           &pt, pt);
-  }
-  if ((int)va >> 12 == 0x821b8) {
-    printf("[[[map %p to %p, pde = %p, pt = %p]]]\n", va, pa, pde, pt);
-  }
+  // 获取页表项
+  PTE *page_table = (PTE *)(PTE_PPN(*page_dir_entry_p) << 12);
+  PTE *page_table_entry_p = &page_table[VPN0(va)];
+  // 检查是不是没有填入过，若填入过则报错
+  assert(!(*page_table_entry_p & 1));
+  // 填入页表项
+  *page_table_entry_p = ((uintptr_t)pa << 10) | 0x1;
 }
 
 Context *ucontext(AddrSpace *as, Area kstack, void *entry) {
