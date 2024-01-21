d52a68757c1f2fad68d73532f98e54586044c0e2
tracer-ics2023 <tracer@njuics.org>
2024-01-21 10:53:14 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:53:14 up  1:31,  1 user,  load average: 1.53, 0.83, 0.72

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -10,9 +10,12 @@
 #define Elf_Ehdr Elf32_Ehdr
 #define Elf_Phdr Elf32_Phdr
 #endif
+extern void *new_page(size_t nr_page);
 uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Ehdr elf_ehdr;
   int fd = fs_open(filename, 0, 0);
+  if (fd == -1)
+    panic("Can not open file %s", filename);
   fs_read(fd, &elf_ehdr, sizeof(Elf_Ehdr));
   // 模数检查
   assert((*(uint32_t *)elf_ehdr.e_ident == 0x464c457f));
@@ -23,10 +26,42 @@ uintptr_t loader(PCB *pcb, const char *filename) {
     if (elf_phdr[i].p_type == 1) {
       // 从ramdisk中读取数据
       fs_lseek(fd, elf_phdr[i].p_offset, SEEK_SET);
-      fs_read(fd, (void *)elf_phdr[i].p_vaddr, elf_phdr[i].p_memsz);
-      // 将未初始化的数据置为0
-      memset((void *)(elf_phdr[i].p_vaddr + elf_phdr[i].p_filesz), 0,
-             elf_phdr[i].p_memsz - elf_phdr[i].p_filesz);
+      uint32_t start = ROUNDDOWN(elf_phdr[i].p_vaddr, PGSIZE);
+      int j = start;
+      for (; j < elf_phdr[i].p_vaddr + elf_phdr[i].p_filesz; j += PGSIZE) {
+        void *page = new_page(1);
+        map(&pcb->as, (void *)j, page, 0);
+        if (j + PGSIZE >= elf_phdr[i].p_vaddr + elf_phdr[i].p_filesz) {
+          fs_read(fd, page, elf_phdr[i].p_vaddr + elf_phdr[i].p_filesz - j);
+          memset(
+              (void *)(page + elf_phdr[i].p_vaddr + elf_phdr[i].p_filesz - j),
+              0, PGSIZE - (elf_phdr[i].p_vaddr + elf_phdr[i].p_filesz - j));
+        } else {
+          fs_read(fd, page, PGSIZE);
+        }
+      }
+      // 未初始化的数据
+      for (; j < elf_phdr[i].p_vaddr + elf_phdr[i].p_memsz; j += PGSIZE) {
+        void *page = new_page(1);
+        map(&pcb->as, (void *)j, page, 0);
+        memset(page, 0, PGSIZE);
+      }
+    }
+  }
+  Elf32_Shdr elf_shdr[elf_ehdr.e_shnum];
+  fs_lseek(fd, elf_ehdr.e_shoff, SEEK_SET);
+  fs_read(fd, elf_shdr, sizeof(Elf32_Shdr) * elf_ehdr.e_shnum);
+  for (int i = 0; i < elf_ehdr.e_shnum; i++) {
+    if (elf_shdr[i].sh_type == 2) {
+      // 符号表
+      Elf32_Sym elf_sym[elf_shdr[i].sh_size / sizeof(Elf32_Sym)];
+      fs_lseek(fd, elf_shdr[i].sh_offset, SEEK_SET);
+      fs_read(fd, elf_sym, elf_shdr[i].sh_size);
+      for (int j = 0; j < elf_shdr[i].sh_size / sizeof(Elf32_Sym); j++) {
+        pcb->max_brk = pcb->max_brk > elf_sym[j].st_value ? pcb->max_brk
+                                                          : elf_sym[j].st_value;
+      }
+      printf("max_brk=%x\n", pcb->max_brk);
     }
   }
   return elf_ehdr.e_entry;
