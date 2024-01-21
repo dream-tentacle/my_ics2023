f81ef372745be7f962f2a1a20da1329072eb5475
tracer-ics2023 <tracer@njuics.org>
2024-01-21 15:16:27 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:16:27 up  5:55,  1 user,  load average: 0.61, 0.66, 0.68

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -81,7 +81,7 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
   if (as->ptr != kas.ptr) {
     printf("map %p to %p, pde = %p, pt = %p\n", va, pa, pde, pt);
   }
-  if ((int)&pde == 0x821dc820) {
+  if ((int)&pde == 0x821b9820) {
     printf("[[map %p to %p, pde = %p, pt = %p]]\n", va, pa, pde, pt);
   }
   if ((int)va >> 12 == 0x821b8) {
