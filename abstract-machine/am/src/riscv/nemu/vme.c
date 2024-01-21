34bd5fe9c0453e21fd455cc7aa2076b0512268c0
tracer-ics2023 <tracer@njuics.org>
2024-01-21 10:11:06 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:11:06 up 49 min,  1 user,  load average: 0.90, 0.97, 0.90

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -66,13 +66,13 @@ void __am_switch(Context *c) {
 int pa_p[1000000], cnt = 0;
 void map(AddrSpace *as, void *va, void *pa, int prot) {
   pa_p[cnt++] = (int)pa;
-  // for (int i = 0; i < cnt - 1; i++) {
-  //   if (pa_p[i] == (int)pa) {
-  //     printf("pa = %p, va = %p\n", pa, va);
-  //     printf("pdir = %p\n", as->ptr);
-  //     assert(0);
-  //   }
-  // }
+  for (int i = 0; i < cnt - 1; i++) {
+    if (pa_p[i] == (int)pa) {
+      printf("pa = %p, va = %p\n", pa, va);
+      printf("pdir = %p\n", as->ptr);
+      assert(0);
+    }
+  }
   // 首先获取页目录项
   PTE *pdir = (PTE *)as->ptr; // 页目录基地址
   uint32_t vpn0 = (uintptr_t)va >> 22;
