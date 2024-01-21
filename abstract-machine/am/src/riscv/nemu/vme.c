1940d39cf3175f5156e578ffebb9f857452b7a0b
tracer-ics2023 <tracer@njuics.org>
2024-01-21 15:33:43 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:33:43 up  6:12,  1 user,  load average: 0.78, 0.78, 0.70

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -12,9 +12,11 @@ static Area segments[] = { // Kernel memory mappings
 
 #define USER_SPACE RANGE(0x40000000, 0x80000000)
 
-static void set_satp(void *pdir) {
+static inline void set_satp(void *pdir) {
   uintptr_t mode = 1ul << (__riscv_xlen - 1);
+  printf("123\n");
   asm volatile("csrw satp, %0" : : "r"(mode | ((uintptr_t)pdir >> 12)));
+  printf("456\n");
 }
 
 static inline uintptr_t get_satp() {
@@ -38,6 +40,7 @@ bool vme_init(void *(*pgalloc_f)(int), void (*pgfree_f)(void *)) {
     }
   }
   set_satp(kas.ptr);
+  printf("789\n");
   vme_enable = 1;
 
   return true;
