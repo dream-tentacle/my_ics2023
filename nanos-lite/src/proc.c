57a5a7d19de31b7187b653b6db257a0f6723ae8b
tracer-ics2023 <tracer@njuics.org>
2024-01-22 13:13:21 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:13:21 up  4:19,  1 user,  load average: 1.44, 0.96, 0.71

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -69,7 +69,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[],
   // pcb->cp->GPRx = (int)sp;
 }
 PCB *add_pcb() {
-  return &pcb[0];
+  return &pcb[1];
   // for (int i = 0; i < MAX_NR_PROC; i++) {
   //   if (pcb[i].cp == NULL) {
   //     return &pcb[i];
@@ -80,9 +80,9 @@ PCB *add_pcb() {
 void init_proc() {
   char *argv[] = {"--skip", NULL};
   char *envp[] = {NULL};
-  protect(&pcb[0].as);
-  context_uload(&pcb[0], "/bin/pal", argv, envp);
-  context_kload(&pcb[1], hello_fun, (void *)"kernel");
+  protect(&pcb[1].as);
+  context_uload(&pcb[1], "/bin/nterm", argv, envp);
+  context_kload(&pcb[0], hello_fun, (void *)"kernel");
   switch_boot_pcb();
   yield();
   // load program here
