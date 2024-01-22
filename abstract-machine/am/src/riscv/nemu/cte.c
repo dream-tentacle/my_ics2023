74d7883217da5ad5dc5e121a2aa40dee7bf75c51
tracer-ics2023 <tracer@njuics.org>
2024-01-22 13:35:50 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:35:50 up  4:42,  1 user,  load average: 0.86, 0.60, 0.76

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -7,15 +7,6 @@ extern void __am_get_cur_as(Context *c);
 extern void __am_switch(Context *c);
 // 一个函数指针，指向一个函数，这个函数的返回值是Context*，参数是Event和Context*。
 Context *__am_irq_handle(Context *c) {
-  printf("-----\nenter handle\n");
-  printf("c=%p\n", c);
-  printf("c->sp = %p\n", c->gpr[2]);
-  printf("c->np = %p\n", c->np);
-  printf("c->mcause = %p\n", c->mcause);
-  printf("c->GPR1 = %p\n", c->GPR1);
-  int mscratch;
-  asm volatile("csrr %0, mscratch" : "=r"(mscratch));
-  printf("mscratch = %p\n", mscratch);
   __am_get_cur_as(c);
   if (user_handler) {
     Event ev = {0};
@@ -44,13 +35,6 @@ Context *__am_irq_handle(Context *c) {
     assert(c != NULL);
   }
   __am_switch(c);
-  printf("c=%p\n", c);
-  printf("c->sp = %p\n", c->gpr[2]);
-  printf("c->np = %p\n", c->np);
-  int sp;
-  asm volatile("mv %0, sp" : "=r"(sp));
-  printf("now sp = %p\n", sp);
-  printf("quit handle\n-----\n");
   return c;
 }
 
