f0a75d060ca9cbf75b569a0fcd1df195ff4b92cd
tracer-ics2023 <tracer@njuics.org>
2024-01-22 13:01:50 +0800

    >  compile NEMU 221240060 熊浚丞 Linux dream-thinkpad 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:01:50 up  4:08,  1 user,  load average: 0.82, 0.90, 0.61

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -296,10 +296,14 @@ static int decode_exec(Decode *s) {
   INSTPAT("0000001 ????? ????? 111 ????? 01100 11", remu, R,
           Assert(src2 != 0, "div by zero in riscv32/inst.c");
           R(rd) = src1 % src2);
-  INSTPAT("??????? ????? ????? 001 ????? 11100 11", csrrw, ZicsrR,
-          R(rd) = csr_read(src2); // src2是csr寄存器编号
-          printf("rd = %d changed to %d\n", rd, R(rd)); csr_write(src2, src1);
-          printf("csr_write %d to csr %d\n", src1, src2););
+  INSTPAT(
+      "??????? ????? ????? 001 ????? 11100 11", csrrw, ZicsrR, do {
+        R(rd) = csr_read(src2); // src2是csr寄存器编号
+        printf("rd = %d changed to %p\n", rd, (void *)(long long)R(rd));
+        csr_write(src2, src1);
+        printf("csr_write %p to csr %p\n", (void *)(long long)src1,
+               (void *)(long long)src2);
+      } while (0));
   INSTPAT("??????? ????? ????? 101 ????? 11100 11", csrrwi, ZicsrI,
           R(rd) = csr_read(src2); // src2是csr寄存器编号
           csr_write(src2, src1));
